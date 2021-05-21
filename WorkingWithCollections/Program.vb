c9fec6ff774510846f1324e14b5f02842e2900c0
bryanIkeNextStep <alexwbailey7@gmail.com>
2021-05-21 16:56:30 -0500

    adding files

--- WorkingWithCollections/Program.vb
+++ WorkingWithCollections/Program.vb
@@ -1,7 +1,18 @@
-Imports System
-
 Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+    Sub Main()
+        Dim car1 As Car = New Car()
+        car1.Make = "Oldsmobile"
+        car1.Model = "Cutlas Supreme"
+
+        Dim car2 As Car = New Car()
+        car2.Make = "Geo"
+        car2.Model = "Prism"
+
+        Dim b1 As Book = New Book()
+        b1.Author = "Robert Tabor"
+        b1.Title = "Microsoft .NET XML Web Services"
+        b1.ISBN = "0-000-00000-0"
+
+        Console.ReadLine()
     End Sub
 End Module
